38b3cbca2c063996cea102d8082d446ba7da0f9f
Jiri Sedlacek <jiri.sedlacek@futu.cz>
2023-06-14 10:16:31 +0200

    Initial commit - copy & paste code from logflare_api_client repo

---
+++ lib/logflare_api_client.ex
@@ -0,0 +1,36 @@
+defmodule LogflareApiClient do
+  @moduledoc false
+
+  @default_api_path "/logs/elixir/logger"
+
+  @callback post_logs(Tesla.Client.t(), list(map), String.t()) ::
+              {:ok, Tesla.Env.t()} | {:error, term}
+
+  @spec new(%{url: String.t(), api_key: String.t()}) :: Tesla.Client.t()
+  def new(%{url: url, api_key: api_key}) when is_binary(url) and is_binary(api_key) do
+    middlewares = [
+      Tesla.Middleware.FollowRedirects,
+      {Tesla.Middleware.Headers,
+       [
+         {"x-api-key", api_key},
+         {"content-type", "application/bert"}
+       ]},
+      {Tesla.Middleware.BaseUrl, url},
+      {Tesla.Middleware.Compression, format: "gzip"}
+    ]
+
+    Tesla.client(
+      middlewares,
+      {Tesla.Adapter.Finch, name: LogflareApiClient.Finch, receive_timeout: 30_000}
+    )
+  end
+
+  @spec post_logs(Tesla.Client.t(), [map], String.t()) :: {:ok, Tesla.Env.t()} | {:error, term}
+  def post_logs(%Tesla.Client{} = client, batch, source_id) when is_list(batch) do
+    body = Bertex.encode(%{"batch" => batch, "source" => source_id})
+
+    Tesla.post(client, api_path(), body)
+  end
+
+  def api_path, do: @default_api_path
+end
---
+++ mix.exs
@@ -0,0 +1,43 @@
+defmodule LogflareApiClient.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :logflare_api_client,
+      version: "0.3.5",
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      description: "A common client interface to the Logflare API.",
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/Logflare/logflare_api_client"}
+      ],
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {LogflareApiClient.Application, []}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:tesla, "~> 1.0"},
+      {:jason, ">= 1.0.0"},
+      {:finch, "~> 0.10"},
+      {:bertex, "~> 1.3"},
+      {:bypass, "~> 2.1", only: :test},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+end
